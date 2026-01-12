b8da91c05f061af8ffa05bd55d79c6ca7879d96e
Ismoilov Narzulla <ismoilovnarzulla08@gmail.com>
2026-01-12 23:15:06 +0500

    Loyiha boshlanishi

---
+++ src/my_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Test module',
+    'description': 'Test module for addons path',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
